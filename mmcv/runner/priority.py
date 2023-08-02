f24dd5afc1ad3a599ea939f48dd60d4643c28f9f
zhangzhenguo <zhangzhenguo@eyecool.cn>
2023-08-02 16:58:51 +0800

    适配昇腾npu的mmcv的打印日志相关的版本降低为1.2.7

--- mmcv/runner/priority.py
+++ mmcv/runner/priority.py
@@ -27,9 +27,7 @@ class Priority(Enum):
     HIGHEST = 0
     VERY_HIGH = 10
     HIGH = 30
-    ABOVE_NORMAL = 40
     NORMAL = 50
-    BELOW_NORMAL = 60
     LOW = 70
     VERY_LOW = 90
     LOWEST = 100
