8d43e2d5ec744a40296366720ac63f01042287af
zhangzhenguo <zhangzhenguo@eyecool.cn>
2023-08-03 11:00:10 +0800

    添加编译并安装的脚本

---
+++ custom_mkinstall.sh
@@ -0,0 +1,9 @@
+export MMCV_WITH_OPS=1
+export MAX_JOBS=8
+source ./test/env_npu.sh
+
+echo "---------build start---------"
+python3 setup.py build_ext
+echo "---------build end---------"
+python3 setup.py develop
+pip3.7 list | grep mmcv
